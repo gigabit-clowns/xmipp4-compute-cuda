0f8f025ba96530656c0df14dbf0e89279629bf49
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2024-11-12 15:15:34 +0100

    Adapting to black box testing

--- tests/integration/src/black_box.cpp
+++ tests/integration/src/black_box.cpp
@@ -27,32 +27,11 @@
 
 
 #include <catch2/catch_test_macros.hpp>
-
-#include <xmipp4/core/plugin_manager.hpp>
-#include <xmipp4/core/plugin.hpp>
-#include <xmipp4/core/platform/operating_system.h>
+#include <xmipp4/core-black-box-tests/test_plugin.hpp>
 
 using namespace xmipp4;
 
-
-static std::string get_cuda_plugin_path()
+TEST_CASE( "black-box test cuda plugin loading", "[compute-cuda]" ) 
 {
-    #if XMIPP4_WINDOWS
-        return "xmipp4-compute-cuda.dll";
-    #elif XMIPP4_APPLE || XMIPP4_LINUX
-        return "./libxmipp4-compute-cuda.so";
-    #else
-        #error "Unknown platform"
-    #endif
-}
-
-TEST_CASE( "load and register xmipp4-compute-cuda plugin", "[compute-cuda]" ) 
-{
-    plugin_manager manager;
-
-    const auto* cuda_plugin = 
-        manager.load_plugin(get_cuda_plugin_path());
-
-    REQUIRE( cuda_plugin != nullptr );
-    REQUIRE( cuda_plugin->get_name() == "xmipp4-compute-cuda" );
+    testing::test_plugin_load("xmipp4-compute-cuda", version(0, 1, 0));
 }
