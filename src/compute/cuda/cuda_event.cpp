71fc19c0677b7b42837df3e4bd750567533e0afd
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2024-11-13 17:59:11 +0100

    Fix codesmell

--- src/compute/cuda/cuda_event.cpp
+++ src/compute/cuda/cuda_event.cpp
@@ -83,7 +83,7 @@ cuda_event::handle cuda_event::get_handle() noexcept
 
 void cuda_event::record()
 {
-    cudaEventRecord(m_event, 0); // TODO check return
+    cudaEventRecord(m_event, nullptr); // TODO check return
 }
 
 void cuda_event::record(device_queue &queue)
